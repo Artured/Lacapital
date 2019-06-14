f7b65a100f245aa70e5d3b9a32fd9a56dd34524b
Arturo <arturohernandez7@icloud.com>
2019-06-13 21:00:35 -0500

    Commit with pimienta negra

---
+++ JS/sub.js
@@ -0,0 +1,7 @@
+var botoncito = document.getElementById("subscription");
+botoncito.addEventListener("click", enviar);
+
+function enviar (evento){
+    alert("Listo! si se hara la carnita asada.");
+    console.log(evento);
+}
